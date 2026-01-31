c84049aa3ac0dcabcd60543a83473055b9434a19
Nick <nicolachiarappa2001@gmail.com>
2026-01-31 16:24:11 +0100

    fix(winner): small fixes

--- src/main/java/it/unicam/coloni/hackhub/context/assessment/domain/repository/WinnerRepository.java
+++ src/main/java/it/unicam/coloni/hackhub/context/assessment/domain/repository/WinnerRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface WinnerRepository extends JpaRepository<Winner, Long> {
     List<Winner> findByEventIdOrderByRankPositionAsc(Long eventId);
-    Optional<Winner> findByEventIdAndTeamId(Long eventId, Long teamId);
+
 
 }
